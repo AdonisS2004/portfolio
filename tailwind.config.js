58fa9729a7aa1a1cce0e4be41fc879eb8b20d4f2
adonisse <aodnisse@mit.edu>
2024-07-23 14:33:05 -0700

    Onboarded tailwind.css using npm

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Adjust the path to match your project's structure
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust as needed for your JS/TS files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: '#78866b',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
