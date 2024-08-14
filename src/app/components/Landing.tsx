5eb24fa573b25424206e291a55726e71e39d8a13
AdonisS2004 <adonisse@mit.edu>
2024-08-14 14:59:08 -0700

    Reactive navigation bar

--- src/app/components/Landing.tsx
+++ src/app/components/Landing.tsx
@@ -1,6 +1,6 @@
 export default function Landing() {
     return (
-        <main className="w-4/5 h-3/4 rounded-xl bg-gray-900 bg-opacity-45 flex justify-center items-center text-white">
+        <main className="w-4/5 h-3/4 rounded-xl bg-neutral-900 bg-opacity-50 z-30 flex justify-center items-center text-white">
             <h1 className="text-4xl">This is where it begins...</h1>
         </main>
     )
--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -6,7 +6,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div>
         <Header />
         <main className="w-screen h-screen bg-landing-image bg-fixed bg-cover flex justify-center items-center">
-            {children}
+          <div className="bg-neutral-900 bg-opacity-20 fixed left-0 top-0 right-0 z-10 w-screen h-screen"></div> {/* opacity overlay for bakcground image */}
+          {children}
         </main>
         <Footer />
     </div>
