ce9d17390d4efefe09bdd221af84e636a1f922a1
AdonisS2004 <adonisse@mit.edu>
2024-08-13 19:23:17 -0700

    Onbaoarded Font Awesome; Tested creating the nav bar;

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className = "fixed top-0 left-0 right-0 z-10 bg-white text-black flex justify-between items-center p-4">
+      <span id="logo"></span>
+      <span id="menu-icon" className = "md:hidden">
+        <FontAwesomeIcon 
+        icon={faBars} 
+        className="text-2xl"
+        />
+      </span>
+      <nav className = "hidden md:flex md:items-center md:gap-x-4">
+        <ul className = "md:flex md:items-center md:gap-x-4">
+          <li className = "hover:text-blue-500"><a href="#">Home</a></li>
+          <li className = "hover:text-blue-500"><a href="#">About</a></li>
+          <li className = "hover:text-blue-500"><a href="#">Resume</a></li>
+          <li className = "hover:text-blue-500"><a href="#">Portfolio</a></li>
+          <li className = "hover:text-blue-500"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
