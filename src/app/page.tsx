8b70436a230a73ebfab9ea02388fd0ad08d64dc6
AdonisS2004 <adonisse@mit.edu>
2024-08-17 06:20:08 -0700

    Adjusted home page to align tiwh Landing page;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Landing from "./pages/landing/page";
 
 export default function Home() {
   return (
-    <main className="w-4/5 h-3/4 rounded-xl bg-neutral-900 bg-opacity-50 z-30 flex justify-center items-center text-white font-customInter">
+    <main className="w-5/6 h-4/5 rounded-xl bg-neutral-900 bg-opacity-70 z-30 flex justify-center items-center text-white font-customInter">
       <Landing />
     </main>
   );
