f977105872f1d08c1c464d770e4dfed7632a113f
AdonisS2004 <adonisse@mit.edu>
2024-08-18 13:54:00 -0700

    Landing Page complete

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     return (
-        <main className="w-5/6 h-4/5 rounded-xl bg-neutral-900 bg-opacity-70 z-30 flex justify-center items-center text-white font-customInter shadow-2xl shadow-slate-200">
+        <main className="w-5/6 h-4/5 overflow-scroll rounded-xl bg-neutral-900 bg-opacity-70 z-30 flex justify-center items-center text-white font-customInter shadow-2xl shadow-slate-200">
             {children}
         </main>
     );
